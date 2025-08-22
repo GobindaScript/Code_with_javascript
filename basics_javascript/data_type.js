433e6ade68a052451b1f302d09a9091ab6c45791
Gobinda Mondal <126812552+GobindaScript@users.noreply.github.com>
2025-08-22 20:36:06 +0000

    commit complete

---
+++ basics_javascript/data_type.js
@@ -0,0 +1,27 @@
+"use strict"//treat all js code as newer version
+
+//alert(4 + 4) we are using node js not browser
+
+console.log(3 + 3)
+
+// follow tc39.es docomentation or mdn docomentation
+ let name = "Gobinda"
+ let age = 20
+ let log = false
+
+ /* data type are
+  1. number => 2 to power 53
+    bigint
+  2.string => ""
+  3.boolean => true/false
+  4.null => standalone value
+  5. undefind =>
+  6.symbol => uniqe
+
+non-premitive
+
+7. object
+*/
+
+console.log(typeof null)
+console.table(typeof [null, undefined, age,])
---
+++ basics_javascript/variable.js
@@ -0,0 +1,15 @@
+const studentid = 42
+let student_email = "gobinda1@gmail.com"
+var accountpass = "Gobinda@123"
+account_city = "kolkata"
+let account_state;
+
+//studentid = 2
+
+//console.log(studentid)
+
+console.table([studentid,student_email,accountpass,account_city, account_state])
+
+/*var is not use currently
+because of the problem of block scope
+*/
