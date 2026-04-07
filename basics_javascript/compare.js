9f5e659f8d85894fc68fc658e3ef0d66f880a3af
Gobinda Mondal <126812552+GobindaScript@users.noreply.github.com>
2026-04-07 12:57:42 +0530

    commit

---
+++ basics_javascript/compare.js
@@ -0,0 +1,13 @@
+console.log(2 < 1)
+console.log(1 > 6)
+console.log(7 <= 8)
+console.log(6 == 6)
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null <= 0);
+console.log(null != 0);
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined <= 0)
